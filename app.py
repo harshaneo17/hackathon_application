d50411829afd206a52e2081c9d52a0b6bb194ed5
harshaneo17 <harsha.god86@gmail.com>
2023-06-24 15:17:45 +0100

    commits to modelpredict, app and requirements

---
+++ app.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from modelpredict import read_image,get_answer,model,processor,show_image
+from fastapi import FastAPI, File, UploadFile
+from io import BytesIO
+
+app = FastAPI() #load fastapi
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File(...)): #type hinting is important https://www.tutorialspoint.com/fastapi/fastapi_type_hints.htm 
+    return {"file_size": len(file)}
+    
+@app.post("/uploadfile/")
+async def main(file: bytes = File(...)): #fastAPI uses pythons type hinting
+    # read image
+    imagem = read_image(file)
+    show_image(file)
+    question = "What is the total or balance?"
+    answer, score = get_answer(imagem, question, model, processor)
+    # transform and get result 
+    return answer,score
---
+++ modelpredict.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from PIL import Image
+import torch
+import re
+from io import BytesIO
+import logging as log
+
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+
+processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-docvqa")
+model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-docvqa")
+
+def read_image(file) -> Image.Image:
+    """ This function takes file and converts it into PIL image format """
+    pil_image = Image.open(BytesIO(file)).convert('RGB')
+    log.info('_____LOADING IMAGE______')
+    return pil_image
+
+def get_answer(filename, question, model, processor):
+
+    image = read_image(filename)
+
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+    
+    prompt = f"<s_docvqa><s_question>{question}</s_question><s_answer>"
+    decoder_input_ids = processor.tokenizer(prompt, add_special_tokens=False, return_tensors="pt")["input_ids"]
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    outputs = model.generate(pixel_values.to(device),
+                               decoder_input_ids=decoder_input_ids.to(device),
+                               max_length=model.decoder.config.max_position_embeddings,
+                               early_stopping=True,
+                               pad_token_id=processor.tokenizer.pad_token_id,
+                               eos_token_id=processor.tokenizer.eos_token_id,
+                               use_cache=True,
+                               num_beams=3,
+                               bad_words_ids=[[processor.tokenizer.unk_token_id]],
+                               return_dict_in_generate=True,
+                               output_scores=True)
+    
+    seq = processor.batch_decode(outputs.sequences)[0]
+    seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+    
+    return processor.token2json(seq), outputs['sequences_scores'].item()*-1
+
+def resultsjson(answer,score) -> list:
+    """ This function takes listobject from the above function and makes it a response object.
+    Returns response object """
+    response = {"class": answer,"confidence": score}
+    return response
+
+def show_image(image):
+    img = image.resize((600,600))
+    img = np.asarray(img)
+    plt.figure(figsize=(7,7))
+    plt.imshow(img)
+
+
+
